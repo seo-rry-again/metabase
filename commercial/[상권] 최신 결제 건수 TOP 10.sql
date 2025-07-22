92120d4960251333a30e4fefec7911ff5f7741ce
seunga lee <106696686+eelb07@users.noreply.github.com>
2025-07-22 20:37:12 +0900

    feat: [상권] 최신 결제 건수 TOP 10
    가장 최근 시간 기준으로 결제 건수가 많은 상위 10개 상권을 보여줍니다. 각 상권의 결제 건수와 최대 결제 금액을 함께 확인할 수 있습니다.

---
+++ commercial/[상권] 최신 결제 건수 TOP 10.sql
@@ -0,0 +1,23 @@
+SELECT
+  a.area_name AS "장소명",
+  a.category AS "카테고리명",
+  c.congestion_level AS "상권 현황",
+  c.total_payment_count AS "결제 건수(신한)",
+  c.payment_amount_max AS "결제 금액(최대)",
+  TO_TIMESTAMP(c.time_key::TEXT, 'YYYYMMDDHH24MI') AS "기준 시각"
+FROM
+  fact.fact_commercial c
+  JOIN dim.dim_area a ON c.area_id = a.area_id
+WHERE
+  TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI') = (
+    SELECT
+      MAX(TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI'))
+    FROM
+      fact.fact_commercial
+    WHERE
+      TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI') <= GETDATE()
+  ) -- 현재 시점과 가장 가까운 기준 시각 하나를 잡아 해당 시각을 기준으로 비교 
+ORDER BY
+  total_payment_count DESC
+LIMIT
+  10;
