e98e90dc74e1d00ac10866ed95028d3100ad7f1f
seunga lee <106696686+eelb07@users.noreply.github.com>
2025-07-22 21:23:00 +0900

    feat: [상권] 결제 건수 상위 지역 내 업종별 구성 현황(최신기준)
     가장 최근 시점 기준 결제 건수가 많은 상위 10개 상권 내 매장들의 업종 구성 알아볼 수 있습니다. 업종별 결제 건수와 최대 결제 금액 평균을 함께 확인할 수 있습니다.

---
+++ commercial/[상권] 결제 건수 상위 지역 내 업종별 구성 현황(최신기준).sql
@@ -0,0 +1,41 @@
+WITH
+  top_rsb AS (
+    SELECT
+      fact_commercial_id
+    FROM
+      fact.fact_commercial
+    WHERE
+      TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI') = (
+        SELECT
+          MAX(TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI'))
+        FROM
+          fact.fact_commercial
+        WHERE
+          TO_TIMESTAMP(time_key::TEXT, 'YYYYMMDDHH24MI') <= GETDATE()
+      )
+    ORDER BY
+      total_payment_count DESC
+    LIMIT
+      10
+  )
+SELECT
+  c.category_large AS "대분류",
+  c.category_medium AS "중분류",
+  SUM(category_payment_count) AS "결제 건수(합계)",
+  AVG(r.category_payment_max) AS "업종별 최대 결제 금액(평균)",
+  TO_TIMESTAMP(r.time_key::TEXT, 'YYYYMMDDHH24MI') AS "기준 시각"
+FROM
+  fact.fact_commercial_rsb r
+  JOIN dim.dim_category c ON r.category_id = c.category_id
+WHERE
+  r.fact_commercial_id IN (
+    SELECT
+      fact_commercial_id
+    FROM
+      top_rsb
+  )
+GROUP BY
+  c.category_large,
+  c.category_medium, r.time_key
+ORDER BY
+  SUM(category_payment_count) DESC;
